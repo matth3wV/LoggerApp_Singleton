cbfc9e21a51d8d16f6668dc7fdde84c3c4396222
Matthew Vernick <mevernick@wpi.edu>
2024-03-19 10:42:38 -0400

    built basic framework for Singleton Logger

--- LoggerApp/Program.cs
+++ LoggerApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,35 @@ namespace LoggerApp
     {
         static void Main(string[] args)
         {
+            Logger e1 = Logger.GetLogger();
         }
+    }
+
+    public class Logger
+    {
+        static Logger _instance;
+
+        private Logger() { }
+
+        private static readonly object _lock = new object();
+
+        public static Logger Instance(string statusType, string value)
+        {
+            if (_instance == null)
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Logger();
+                        _instance.StatusType = statusType;
+                        _instance.Value = value;
+                    }
+                }
+            }
+            return _instance;
+        }
+        public string StatusType { get; set; }
+        public string Value { get; set; }
     }
 }
